b890ccfb09e4a1cf19b0c48d932f7f54ee7b9b7d
Herwig Hochleitner <hhochleitner@gmail.com>
2013-02-13 10:37:02 +0100

    version 0

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject webnf "0.0.0"
+  :description "the web non framework"
+  :url "http://github.com/webnf/webnf"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
