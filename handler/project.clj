09809730b568d7f546bfc0623b4002693d823f57
Herwig Hochleitner <hhochleitner@gmail.com>
2013-09-09 11:11:06 +0200

    revert to enlive 1.1.0 due to regression in lockstep transform

--- handler/project.clj
+++ handler/project.clj
@@ -7,8 +7,9 @@
   :dependencies [[webnf/base "0.0.2"]
                  [webnf/async-servlet "0.0.1"]
                  
-                 [enlive "1.1.4"]
-                 [net.cgrand/moustache "1.1.0"]
+                 [enlive "1.1.0"]
+                 [net.cgrand/moustache "1.1.0"
+                  :exclusions [ring/ring-core]]
                  [ring/ring-core "1.2.0"]
                  [ring-mock "0.1.5"]
 
