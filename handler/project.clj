f51f148a39af03288abfcf24bf71b4a79a7b851a
Herwig Hochleitner <hhochleitner@gmail.com>
2013-09-12 21:17:18 +0200

    v0.0.2

--- handler/project.clj
+++ handler/project.clj
@@ -1,4 +1,4 @@
-(defproject webnf/handler "0.0.1"
+(defproject webnf/handler "0.0.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
