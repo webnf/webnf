43b5d1fafdcda369d214aeacd79067e0591808c5
Herwig Hochleitner <herwig@bendlas.net>
2013-10-15 02:39:02 +0200

    Updated deps

--- handler/project.clj
+++ handler/project.clj
@@ -1,11 +1,11 @@
-(defproject webnf/handler "0.0.3"
+(defproject webnf/handler "0.0.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
-  :dependencies [[webnf/base "0.0.3"]
-                 [webnf/async-servlet "0.0.1"]
+  :dependencies [[webnf/base "0.0.4"]
+                 [webnf/async-servlet "0.0.2"]
                  
                  [enlive "1.1.1"]
                  [net.cgrand/moustache "1.1.0"
@@ -13,5 +13,5 @@
                  [ring/ring-core "1.2.0"]
                  [ring-mock "0.1.5"]
 
-                 [org.mindrot/jbcrypt "0.3m"]
+                 [com.lambdaworks/scrypt "1.4.0"]
                  [nl.bitwalker/UserAgentUtils "1.2.4"]])
