01ff02af6b2dca43c1df3ad1d246ec7e9c700b4d
Herwig Hochleitner <herwig@bendlas.net>
2014-09-04 04:44:53 +0200

    Add helper to print out class definition in java-like syntax

--- base/src/webnf/base.clj
+++ base/src/webnf/base.clj
@@ -94,3 +94,57 @@
   [bindings body]
   `(apply concat (for ~bindings ~body)))
 
+(autoload clojure.reflect/reflect)
+
+(defn pr-cls [^Class cls & {:as flags :keys [public private protected static fields methods declaring bases final abstract]}]
+  (let [{:as want-flags? :keys [public private protected static fields methods declaring bases abstract]}
+        (merge {:public true :private false :protected false :static true
+                :fields true :methods true :bases true :declaring false
+                :final true :abstract true}
+               flags)
+        rc (reflect cls :ancestors bases)
+        want-member? (fn [{fl :flags dc :declaring-class pt :parameter-types :as mt}]
+                       ;; (log/trace (:name mt) dc (keys mt) (:flags mt))
+                       (cond #_#_ (and (not= (name dc) (.getName cls))
+                                       (not bases))
+                             false
+                             (and (not pt) (not fields))
+                             false
+                             :else (every? want-flags?
+                                           (if (or (:public fl)
+                                                   (:private fl))
+                                             (seq fl)
+                                             (cons :protected fl)))))
+        method-key (fn [{:keys [name return-type type declaring-class
+                                parameter-types exception-types flags]}]
+                                        ; fields > constructors > methods
+                     [(cond (not parameter-types) 0
+                            (not return-type) 1
+                            :else 2)
+                      (cond (:public flags) 0
+                            (:private flags) 2
+                            :else 1)
+                      (if (:static flags) 1 0)
+                      name])]
+    (println
+     (apply 
+      str (concat (interpose " " (map name (:flags rc))) [" class "] [(.getName cls)]
+                  (when (:bases rc)
+                    (interpose " " (cons " extends" (map str (:bases rc)))))
+                  [" {\n"]
+                  (forcat [{:keys [name return-type type declaring-class
+                                   parameter-types exception-types flags]}
+                           (sort-by method-key (filter want-member? (:members rc)))]
+                          (concat
+                           ["    "]
+                           (interleave (map clojure.core/name flags) (repeat " "))
+                           [(or return-type type "new")
+                            " " name]
+                           (when parameter-types
+                             (concat ["("] (interpose ", " parameter-types) [")"]
+                                     (when (seq exception-types)
+                                       (cons " throws " (interpose ", " exception-types)))))
+                           [";"]
+                           (when declaring [" // from " (str declaring-class)])
+                           ["\n"]))
+                  ["}\n"])))))
