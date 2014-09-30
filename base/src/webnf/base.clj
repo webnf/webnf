89bc7815771b880195308c1fe402f7a7b28350ab
Herwig Hochleitner <herwig@bendlas.net>
2014-09-30 05:49:56 +0200

    export pr-cls

--- base/src/webnf/base.clj
+++ base/src/webnf/base.clj
@@ -6,7 +6,5 @@
 (autoload ^:macro ^:static webnf.base.autoload/autoload-some)
 (autoload ^:static clojure.pprint/pprint)
 (autoload-some ^:static (clojure.repl pst source doc))
-(autoload-some ^:static (webnf.base.platform hostname reset-logging-config!))
+(autoload-some ^:static (webnf.base.platform hostname reset-logging-config! pr-cls))
 (autoload-some ^:static (webnf.base.utils to-many squelch pprint-str ^:macro forcat) )
-
-
