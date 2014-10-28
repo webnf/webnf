50c4ffb4a60a30136c7a09fd921c70b267a558a5
Herwig Hochleitner <herwig@bendlas.net>
2014-10-28 08:21:26 +0100

    Add nrepl utils to cljs.deps/dev, 0.1.0-alpha2

--- deps.dev/project.clj
+++ deps.dev/project.clj
@@ -1,7 +1,11 @@
-(defproject webnf.deps/dev "0.1.0-SNAPSHOT"
+(defproject webnf.deps/dev "0.1.0-alpha2"
   :plugins [[lein-modules "0.3.9"]]
   :description "Development dependencies"
   :dependencies [[clj-stacktrace "0.2.8"]
                  [clojure-complete "0.2.4"]
                  [criterium "0.4.3"]
-                 [prone "0.6.0"]])
+                 [lein-light-nrepl "0.0.18"]
+                 [org.clojure/tools.reader "0.8.10"] ; override reader from lein-light-repl
+                 [cider/cider-nrepl "0.7.0"]
+                 [com.cemerick/piggieback "0.1.3"]
+                 [weasel "0.4.2"]])
