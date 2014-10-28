982fd0150dd4b91a71c8f59224b1b54c16d201c9
Herwig Hochleitner <herwig@bendlas.net>
2014-10-28 15:35:20 +0100

    server and handler 0.1.0-alpha2

--- handler/project.clj
+++ handler/project.clj
@@ -1,4 +1,4 @@
-(defproject webnf/handler "0.1.0-SNAPSHOT"
+(defproject webnf/handler "0.1.0-alpha2"
   :plugins [[lein-modules "0.3.9"]]
   :description "The first minor release of webnf handler, a collection
   of code to aid web handler development. There is:
