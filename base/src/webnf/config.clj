925e4ca348cd125d7a01b307645fd822178ecd3b
Herwig Hochleitner <herwig@bendlas.net>
2015-02-25 21:11:14 +0100

    add webnf.config for configuration handling

---
+++ base/src/webnf/config.clj
@@ -0,0 +1,19 @@
+(ns webnf.config)
+
+;; Allow configuration to be defined even before this namespace is loaded
+(defonce configuration ::unbound)
+
+(defn set-configuration! [cfg]
+  (when (not= ::unbound configuration)
+    (throw (ex-info "Runtime configuration already bound" {:var #'configuration
+                                                           :cfg cfg})))
+  (alter-var-root #'configuration (constantly cfg)))
+
+(defn update-with-defaults!
+  ([default-config] (update-with-defaults! default-config merge))
+  ([default-config merge-fn]
+   (alter-var-root
+    #'configuration (constantly
+                     (if (= ::unbound #'configuration)
+                       default-config
+                       (merge-fn default-config configuration))))))
