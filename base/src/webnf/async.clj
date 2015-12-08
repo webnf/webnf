a2d27883e37454a100b806595742ef3cd33b1289
Herwig Hochleitner <herwig@bendlas.net>
2015-12-08 04:19:21 +0100

    rw-chan

--- base/src/webnf/async.clj
+++ base/src/webnf/async.clj
@@ -1,5 +1,9 @@
 (ns webnf.async
-  (:require [clojure.core.async :refer [<!!]]))
+  (:require
+   [clojure.core.async :refer [<!!]]
+   [clojure.core.async.impl.protocols :refer
+    [ReadPort WritePort Channel
+     take! put! close! closed?]]))
 
 (defn chan-seq
   "A lazy seq that gets its elements by taking from chan."
@@ -7,3 +11,19 @@
   (lazy-seq
    (when-let [x (<!! ch)]
      (cons x (chan-seq ch)))))
+
+(defn rw-chan [read-port write-port]
+  (reify
+    Channel
+    (close! [_]
+      (close! write-port)
+      (close! read-port))
+    (closed? [_]
+      (or (closed? write-port)
+          (closed? read-port)))
+    ReadPort
+    (take! [_ fn-handler]
+      (take! read-port fn-handler))
+    WritePort
+    (put! [_ val fn-handler]
+      (put! write-port val fn-handler))))
