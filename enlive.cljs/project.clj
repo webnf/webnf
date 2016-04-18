50df2cc10bdc0f05275bd4e3c3240e437cab7df9
Herwig Hochleitner <herwig@bendlas.net>
2016-04-18 09:13:38 +0200

    remove enlive.cljs subproject

--- enlive.cljs/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject webnf/enlive.cljs "0.2.0-SNAPSHOT"
-  :plugins [[lein-modules "0.3.11"]]
-  :description "ClojureScript port of enlive"
-  :dependencies [[webnf/cljs "_" :upgrade false]])
