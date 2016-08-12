c5571a4bfe7b10851735c18355c4178ba8d67814
Herwig Hochleitner <herwig@bendlas.net>
2016-08-12 21:03:28 +0200

    BREAKING: add date parser
    rename webnf.date/simple-date-format -> webnf.date/simple-date-formatter

--- handler/src/clj/webnf/date.clj
+++ handler/src/clj/webnf/date.clj
@@ -3,24 +3,47 @@
            (java.text SimpleDateFormat DateFormat)))
 
 (defn thread-local-date-format
-  "Make a thread-safe formatter function from a DateFormat factory fn
-  
+  "Make a thread-safe DateFormat from a DateFormat factory fn
+
   SimpleDateFormat is not thread-safe, so we use a ThreadLocal proxy for access.
   http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4228335"
-  ([make-df]
-   (let [tldf (proxy [ThreadLocal] []
-                (initialValue [] (make-df)))]
-     (fn format-date [date]
-       (.format ^DateFormat (.get ^ThreadLocal tldf) date)))))
+  [make-df] (proxy [ThreadLocal] []
+              (initialValue [] (make-df))))
 
-(defn simple-date-format
+(defn date-formatter
+  "Make a thread-safe formatter function from a DateFormat factory fn
+
+  SimpleDateFormat is not thread-safe, so we use a ThreadLocal proxy for access.
+  http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4228335"
+  [tldf] (fn format-date [date]
+           (.format ^DateFormat (.get ^ThreadLocal tldf) date)))
+
+(defn date-parser
+  "Make a thread-safe parser function from a DateFormat factory fn
+
+  SimpleDateFormat is not thread-safe, so we use a ThreadLocal proxy for access.
+  http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4228335"
+  [tldf] (fn parse-date [string]
+           (.parse ^DateFormat (.get ^ThreadLocal tldf) string)))
+
+(defn simple-date-formatter
   "Make a thread-safe date formatter based on SimpleDateFormat"
-  ([fmt] (simple-date-format fmt Locale/US (TimeZone/getTimeZone "GMT")))
+  ([fmt] (simple-date-formatter fmt Locale/US (TimeZone/getTimeZone "GMT")))
   ([fmt locale timezone]
-   (thread-local-date-format
-    #(doto (SimpleDateFormat. fmt ^Locale locale)
-       (.setTimeZone timezone)))))
+   (date-formatter
+    (thread-local-date-format
+     #(doto (SimpleDateFormat. fmt ^Locale locale)
+        (.setTimeZone timezone))))))
+
+(defn simple-date-parser
+  "Make a thread-safe date-parser based on SimpleDateFormat"
+  ([fmt] (simple-date-parser fmt Locale/US (TimeZone/getTimeZone "GMT")))
+  ([fmt locale timezone]
+   (date-parser
+    (thread-local-date-format
+     #(doto (SimpleDateFormat. fmt ^Locale locale)
+        (.setTimeZone timezone))))))
 
 ;; RFC3339 says to use -00:00 when the timezone is unknown (+00:00 implies a known GMT)
-(def format-utc  (simple-date-format "yyyy-MM-dd'T'HH:mm:ss.SSS-00:00"))
-(def format-http (simple-date-format "EEE, dd MMM yyyy HH:mm:ss z"))
+(def format-utc  (simple-date-formatter "yyyy-MM-dd'T'HH:mm:ss.SSS-00:00"))
+(def format-http (simple-date-formatter "EEE, dd MMM yyyy HH:mm:ss z"))
