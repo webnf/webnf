0b4d85da862c0ce3e4aed1346e4eb7237b4eea2d
Herwig Hochleitner <herwig@bendlas.net>
2017-07-24 18:29:36 +0200

    xhr: split xhr and xhr-async [breaking]

--- js/src/webnf/js/xhr.cljs
+++ js/src/webnf/js/xhr.cljs
@@ -6,7 +6,8 @@
             [webnf.base.logging :as log]
             [clojure.string :refer [split-lines]]
             [webnf.js :refer [to-js]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.spec.alpha :as s]))
 
 ;; ### XHR
 
@@ -24,26 +25,71 @@
                  res)))
            (transient {}) (split-lines hs))))
 
-(defn- parse-xhr-response [evt]
-  (let [t (.-target evt)]
-    {:uri (.getLastUri t)
-     :status (.getStatus t)
-     :headers (hmap (.getAllResponseHeaders t))
-     :body (.getResponseText t)}))
-
-(defn- parse-xhr-response-xml [evt]
-  (let [t (.-target evt)]
-    {:uri (.getLastUri t)
-     :status (.getStatus t)
-     :headers (hmap (.getAllResponseHeaders t))
-     :body (.getResponseXml t)}))
-
 (defn- as-str [o]
   (if (keyword? o)
     (name o)
     (str o)))
 
+(s/def ::header-name string?) ;; TODO specify allowed characters
+
+(s/def ::uri
+  (s/conformer #(try (Uri/parse %)
+                     (catch :default e ::s/invalid))
+               #(.toString %)))
+
+(s/def ::method (s/or :str (s/and string? #(re-matches #"[a-zA-Z]*" %))
+                      :kw  (s/and keyword? #(re-matches #"[a-zA-Z]*" (name %)))))
+(s/def ::params  (s/map-of string? string?))
+(s/def ::headers (s/map-of ::header-name string?))
+(s/def :webnf.xhr.request/body string?)
+(s/def :webnf.xhr.options/xml boolean?)
+
+(s/def ::options
+  (s/keys
+   :opt-un [::method ::params ::headers
+            :webnf.xhr.request/body
+            :webnf.xhr.options/xml]))
+
+(s/def ::status (s/int-in 100 900))
+(s/def ::xml-doc #(instance? js/XMLDocument %))
+
+(s/def ::response-callback (s/fspec
+                            :args (s/cat
+                                   :status  ::http-status
+                                   :headers ::headers
+                                   :body    (s/or :text-body string? :xml-body ::xml-doc)
+                                   :xhr     #(instance? js/XMLHttpRequest %))))
+
+(s/fdef xhr
+        :args (s/cat
+               :request-uri ::uri
+               :request-data (s/? ::options)
+               :on-finish ::response-callback))
+
 (defn xhr
+  "Request an uri with clojure data, via XMLHttpRequest.
+
+  Takes a callback, that will be called with [status headers body], returns a goog.net.XhrIo object.
+  Takes headers and params as clojure data, passes headers as clojure data into callback."
+  ([uri on-finish] (xhr uri nil on-finish))
+  ([uri
+    {:keys [method body headers params xml]}
+    on-finish]
+   (let [uri (Uri/parse uri)
+         _ (reduce-kv (fn [_ param value]
+                        (.setParameterValue uri param value))
+                      nil params)
+         headers (and headers (to-js headers))]
+     (XhrIo/send uri #(let [t (.-target %)]
+                        (on-finish (.getStatus t)
+                                   (hmap (.getAllResponseHeaders t))
+                                   (if xml
+                                     (.getResponseXml t)
+                                     (.getResponseText t))
+                                   t))
+                 (as-str method) body headers))))
+
+(defn xhr-async
   "Request an uri with XmlHttpRequest, return channel that will
   receive response in ring format {:uri :status :headers :body}
 
@@ -52,17 +98,16 @@
   :auto-refresh true triggers a request on every read
   :parse-response can be a custom response parser working directly on the XmlHttpRequest"
   ([uri] (xhr uri nil))
-  ([uri {:keys [method body headers params auto-refresh parse-response xml]}]
-   (let [uri (Uri/parse uri)
-         parser (if parse-response
-                  (comp parse-response #(.-target %))
-                  (if xml parse-xhr-response-xml parse-xhr-response))
-         _ (reduce-kv (fn [_ param value]
-                        (.setParameterValue uri param value))
-                      nil params)
-         headers (and headers (to-js headers))
-         rp (callback-read-port (fn [result]
-                                  (XhrIo/send uri (comp result parser)
-                                              (as-str method) body headers)))]
+  ([uri {:keys [method body headers params auto-refresh parse-response xml] :as options}]
+   (let [rp (callback-read-port (fn [result]
+                                  (xhr uri options
+                                       (fn [status headers body target]
+                                         (result
+                                          (if parse-response
+                                            (parse-response target)
+                                            {:uri (.getLastUri target)
+                                             :status status
+                                             :headers headers
+                                             :body body}))))))]
      (if auto-refresh
        rp (promise rp)))))
